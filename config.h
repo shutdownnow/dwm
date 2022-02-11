76cabdb5a59e3f2dfd0c01ae51a14d83a26facb0
Nobume <923551363@qq.com>
2022-02-11 09:51:52 +0800

    alpha

--- config.h
+++ config.h
@@ -12,12 +12,19 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = {"#aaaaaa", "#222B2E", "#222B2E"},
 	[SchemeSel] = {"#000000", "#2EB398", "#2EB398"},
 	/*[SchemeUrg] = {"#000000", "#F46067", "#F46067"},*/
 	/*[SchemeTitle] = {"#ffffff", "#222B2E", "#222B2E"},*/
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
